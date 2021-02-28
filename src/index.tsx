f28844376fd29398ce217d38beb26bc007b803bb
vishtree <vishalnarkhede.iitd@gmail.com>
2021-02-28 17:47:29 +0100

    feat: added forwardRef support
    https://github.com/GetStream/react-native-bidirectional-infinite-scroll/issues/3

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { MutableRefObject, useRef, useState } from 'react';
 import {
   ActivityIndicator,
+  FlatList as FlatListType,
   FlatListProps,
   ScrollViewProps,
   StyleSheet,
@@ -66,176 +67,185 @@ type Props<T> = Omit<FlatListProps<T>, 'maintainVisibleContentPosition'> & {
  * - doesn't accept `ListHeaderComponent` via prop, since it is occupied by `HeaderLoadingIndicator`
  *    Set `showDefaultLoadingIndicators` to use `ListHeaderComponent`.
  */
-const BidirectionalFlatList = <T extends any>(props: Props<T>) => {
-  const {
-    activityIndicatorColor = 'black',
-    data,
-    enableAutoscrollToTop,
-    autoscrollToTopThreshold = 100,
-    FooterLoadingIndicator,
-    HeaderLoadingIndicator,
-    ListHeaderComponent,
-    ListFooterComponent,
-    onEndReached = () => Promise.resolve(),
-    onEndReachedThreshold = 10,
-    onScroll,
-    onStartReached = () => Promise.resolve(),
-    onStartReachedThreshold = 10,
-    showDefaultLoadingIndicators = true,
-  } = props;
+const BidirectionalFlatList = React.forwardRef(
+  (
+    // TODO: Fix typescript generics for ref forwarding.
+    props: Props<any>,
+    ref:
+      | ((instance: FlatListType | null) => void)
+      | MutableRefObject<FlatListType | null>
+      | null
+  ) => {
+    const {
+      activityIndicatorColor = 'black',
+      data,
+      enableAutoscrollToTop,
+      autoscrollToTopThreshold = 100,
+      FooterLoadingIndicator,
+      HeaderLoadingIndicator,
+      ListHeaderComponent,
+      ListFooterComponent,
+      onEndReached = () => Promise.resolve(),
+      onEndReachedThreshold = 10,
+      onScroll,
+      onStartReached = () => Promise.resolve(),
+      onStartReachedThreshold = 10,
+      showDefaultLoadingIndicators = true,
+    } = props;
+    const [onStartReachedInProgress, setOnStartReachedInProgress] = useState(
+      false
+    );
+    const [onEndReachedInProgress, setOnEndReachedInProgress] = useState(false);
 
-  const [onStartReachedInProgress, setOnStartReachedInProgress] = useState(
-    false
-  );
-  const [onEndReachedInProgress, setOnEndReachedInProgress] = useState(false);
+    const onStartReachedTracker = useRef<Record<number, boolean>>({});
+    const onEndReachedTracker = useRef<Record<number, boolean>>({});
 
-  const onStartReachedTracker = useRef<Record<number, boolean>>({});
-  const onEndReachedTracker = useRef<Record<number, boolean>>({});
+    const onStartReachedInPromise = useRef<Promise<void> | null>(null);
+    const onEndReachedInPromise = useRef<Promise<void> | null>(null);
 
-  const onStartReachedInPromise = useRef<Promise<void> | null>(null);
-  const onEndReachedInPromise = useRef<Promise<void> | null>(null);
+    const maybeCallOnStartReached = () => {
+      // If onStartReached has already been called for given data length, then ignore.
+      if (data?.length && onStartReachedTracker.current[data.length]) {
+        return;
+      }
 
-  const maybeCallOnStartReached = () => {
-    // If onStartReached has already been called for given data length, then ignore.
-    if (data?.length && onStartReachedTracker.current[data.length]) {
-      return;
-    }
+      if (data?.length) {
+        onStartReachedTracker.current[data.length] = true;
+      }
 
-    if (data?.length) {
-      onStartReachedTracker.current[data.length] = true;
-    }
+      setOnStartReachedInProgress(true);
+      const p = () => {
+        return new Promise<void>((resolve) => {
+          onStartReachedInPromise.current = null;
+          setOnStartReachedInProgress(false);
+          resolve();
+        });
+      };
 
-    setOnStartReachedInProgress(true);
-    const p = () => {
-      return new Promise<void>((resolve) => {
-        onStartReachedInPromise.current = null;
-        setOnStartReachedInProgress(false);
-        resolve();
-      });
-    };
-
-    if (onEndReachedInPromise.current) {
-      onEndReachedInPromise.current.finally(() => {
+      if (onEndReachedInPromise.current) {
+        onEndReachedInPromise.current.finally(() => {
+          onStartReachedInPromise.current = onStartReached().then(p);
+        });
+      } else {
         onStartReachedInPromise.current = onStartReached().then(p);
-      });
-    } else {
-      onStartReachedInPromise.current = onStartReached().then(p);
-    }
-  };
-
-  const maybeCallOnEndReached = () => {
-    // If onEndReached has already been called for given data length, then ignore.
-    if (data?.length && onEndReachedTracker.current[data.length]) {
-      return;
-    }
-
-    if (data?.length) {
-      onEndReachedTracker.current[data.length] = true;
-    }
-
-    setOnEndReachedInProgress(true);
-    const p = () => {
-      return new Promise<void>((resolve) => {
-        onStartReachedInPromise.current = null;
-        setOnStartReachedInProgress(false);
-        resolve();
-      });
+      }
     };
 
-    if (onStartReachedInPromise.current) {
-      onStartReachedInPromise.current.finally(() => {
+    const maybeCallOnEndReached = () => {
+      // If onEndReached has already been called for given data length, then ignore.
+      if (data?.length && onEndReachedTracker.current[data.length]) {
+        return;
+      }
+
+      if (data?.length) {
+        onEndReachedTracker.current[data.length] = true;
+      }
+
+      setOnEndReachedInProgress(true);
+      const p = () => {
+        return new Promise<void>((resolve) => {
+          onStartReachedInPromise.current = null;
+          setOnStartReachedInProgress(false);
+          resolve();
+        });
+      };
+
+      if (onStartReachedInPromise.current) {
+        onStartReachedInPromise.current.finally(() => {
+          onEndReachedInPromise.current = onEndReached().then(p);
+        });
+      } else {
         onEndReachedInPromise.current = onEndReached().then(p);
-      });
-    } else {
-      onEndReachedInPromise.current = onEndReached().then(p);
-    }
-  };
-
-  const handleScroll: ScrollViewProps['onScroll'] = (event) => {
-    // Call the parent onScroll handler, if provided.
-    onScroll?.(event);
-
-    const offset = event.nativeEvent.contentOffset.y;
-    const visibleLength = event.nativeEvent.layoutMeasurement.height;
-    const contentLength = event.nativeEvent.contentSize.height;
-
-    // Check if scroll has reached either start of end of list.
-    const isScrollAtStart = offset < onStartReachedThreshold;
-    const isScrollAtEnd =
-      contentLength - visibleLength - offset < onEndReachedThreshold;
-
-    if (isScrollAtStart) {
-      maybeCallOnStartReached();
-    }
-
-    if (isScrollAtEnd) {
-      maybeCallOnEndReached();
-    }
-  };
-
-  const renderHeaderLoadingIndicator = () => {
-    if (!showDefaultLoadingIndicators) {
-      if (ListHeaderComponent) {
-        return <ListHeaderComponent />;
-      } else {
-        return null;
       }
-    }
+    };
 
-    if (!onStartReachedInProgress) return null;
+    const handleScroll: ScrollViewProps['onScroll'] = (event) => {
+      // Call the parent onScroll handler, if provided.
+      onScroll?.(event);
 
-    if (HeaderLoadingIndicator) {
-      return <HeaderLoadingIndicator />;
-    }
+      const offset = event.nativeEvent.contentOffset.y;
+      const visibleLength = event.nativeEvent.layoutMeasurement.height;
+      const contentLength = event.nativeEvent.contentSize.height;
+
+      // Check if scroll has reached either start of end of list.
+      const isScrollAtStart = offset < onStartReachedThreshold;
+      const isScrollAtEnd =
+        contentLength - visibleLength - offset < onEndReachedThreshold;
+
+      if (isScrollAtStart) {
+        maybeCallOnStartReached();
+      }
+
+      if (isScrollAtEnd) {
+        maybeCallOnEndReached();
+      }
+    };
+
+    const renderHeaderLoadingIndicator = () => {
+      if (!showDefaultLoadingIndicators) {
+        if (ListHeaderComponent) {
+          return <ListHeaderComponent />;
+        } else {
+          return null;
+        }
+      }
+
+      if (!onStartReachedInProgress) return null;
+
+      if (HeaderLoadingIndicator) {
+        return <HeaderLoadingIndicator />;
+      }
+
+      return (
+        <View style={styles.indicatorContainer}>
+          <ActivityIndicator size={'small'} color={activityIndicatorColor} />
+        </View>
+      );
+    };
+
+    const renderFooterLoadingIndicator = () => {
+      if (!showDefaultLoadingIndicators) {
+        if (ListFooterComponent) {
+          return <ListFooterComponent />;
+        } else {
+          return null;
+        }
+      }
+
+      if (!onEndReachedInProgress) return null;
+
+      if (FooterLoadingIndicator) {
+        return <FooterLoadingIndicator />;
+      }
+
+      return (
+        <View style={styles.indicatorContainer}>
+          <ActivityIndicator size={'small'} color={activityIndicatorColor} />
+        </View>
+      );
+    };
 
     return (
-      <View style={styles.indicatorContainer}>
-        <ActivityIndicator size={'small'} color={activityIndicatorColor} />
-      </View>
+      <>
+        <FlatList
+          {...props}
+          ref={ref}
+          progressViewOffset={50}
+          ListHeaderComponent={renderHeaderLoadingIndicator}
+          ListFooterComponent={renderFooterLoadingIndicator}
+          onEndReached={null}
+          onScroll={handleScroll}
+          // @ts-ignore
+          maintainVisibleContentPosition={{
+            autoscrollToTopThreshold: enableAutoscrollToTop
+              ? autoscrollToTopThreshold
+              : undefined,
+            minIndexForVisible: 1,
+          }}
+        />
+      </>
     );
-  };
-
-  const renderFooterLoadingIndicator = () => {
-    if (!showDefaultLoadingIndicators) {
-      if (ListFooterComponent) {
-        return <ListFooterComponent />;
-      } else {
-        return null;
-      }
-    }
-
-    if (!onEndReachedInProgress) return null;
-
-    if (FooterLoadingIndicator) {
-      return <FooterLoadingIndicator />;
-    }
-
-    return (
-      <View style={styles.indicatorContainer}>
-        <ActivityIndicator size={'small'} color={activityIndicatorColor} />
-      </View>
-    );
-  };
-
-  return (
-    <>
-      <FlatList
-        {...props}
-        progressViewOffset={50}
-        ListHeaderComponent={renderHeaderLoadingIndicator}
-        ListFooterComponent={renderFooterLoadingIndicator}
-        onEndReached={null}
-        onScroll={handleScroll}
-        // @ts-ignore
-        maintainVisibleContentPosition={{
-          autoscrollToTopThreshold: enableAutoscrollToTop
-            ? autoscrollToTopThreshold
-            : undefined,
-          minIndexForVisible: 1,
-        }}
-      />
-    </>
-  );
-};
+  }
+);
 
 export { BidirectionalFlatList as FlatList };
