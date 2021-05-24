25e1fa8e67648e03034a15f2cbfcfeee4035d114
vishtree <vishalnarkhede.iitd@gmail.com>
2021-05-24 11:47:08 +0200

    onrefresh functionality

--- example/src/MessageListExample.tsx
+++ example/src/MessageListExample.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import { FlatList } from 'react-native-bidirectional-infinite-scroll';
 import { MessageBubble } from './MessageBubble';
 import { Message, queryMoreMessages } from './utils';
 
+const ItemSeparatorComponent = () => <View style={{
+  borderBottomColor: 'black',
+  height: 0,
+  borderBottomWidth: 1,
+  marginVertical: 10
+}} />;
+
 const App = () => {
   const [messages, setMessages] = useState<Array<Message>>([]);
+  const flRef = useRef(null);
   useEffect(() => {
     const initChat = async () => {
-      const initialMessages = await queryMoreMessages(50);
+      const initialMessages = await queryMoreMessages(50, null, null);
       if (!initialMessages) return;
 
       setMessages(initialMessages);
@@ -19,18 +27,19 @@ const App = () => {
   }, []);
 
   const loadMoreOlderMessages = async () => {
-    const newMessages = await queryMoreMessages(10);
-    setMessages((m) => {
-      return m.concat(newMessages);
-    });
+      const newMessages = await queryMoreMessages(10, null, messages[messages.length - 1].id);
+      setMessages((m) => {
+        return m.concat(newMessages);
+      });
   };
 
-  const loadMoreRecentMessages = async () => {
-    const newMessages = await queryMoreMessages(10);
+  const loadMoreRecentMessages = useCallback(async () => {
+    const newMessages = await queryMoreMessages(10, messages[0].id, null);
+    console.log('loadMoreRecentMessages before ', messages[0].id)
     setMessages((m) => {
       return newMessages.concat(m);
     });
-  };
+  }, [messages]);
 
   if (!messages.length) {
     return null;
@@ -43,9 +52,11 @@ const App = () => {
       </View>
       <FlatList
         data={messages}
-        inverted
+        ref={flRef}
         onEndReached={loadMoreOlderMessages}
         onStartReached={loadMoreRecentMessages}
+        // inverted
+        // onRefresh={loadMoreRecentMessages}
         renderItem={MessageBubble}
       />
     </SafeAreaView>
--- src/BidirectionalFlatList.tsx
+++ src/BidirectionalFlatList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import React, {
   MutableRefObject,
   useEffect,
@@ -7,7 +8,9 @@ import React, {
 } from 'react';
 import {
   ActivityIndicator,
+  Animated,
   FlatList as FlatListType,
+  PanResponder,
   StyleSheet,
   View,
 } from 'react-native';
@@ -20,8 +23,22 @@ const styles = StyleSheet.create({
     paddingVertical: 5,
     width: '100%',
   },
+  refreshControl: {
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    position: 'absolute',
+    width: '100%',
+  },
 });
 
+const waiter = () => new Promise((resolve) => setTimeout(resolve, 500));
+const dampingFactor = 5;
+
+type WebFlatListProps<T> = Props<T> & {
+  onScroll: (event: React.UIEvent<'div', UIEvent>) => void;
+};
 /**
  * Note:
  * - `onEndReached` and `onStartReached` must return a promise.
@@ -35,7 +52,7 @@ const styles = StyleSheet.create({
 // eslint-disable-next-line react/display-name
 export const BidirectionalFlatList = (React.forwardRef(
   <T extends unknown>(
-    props: Props<T>,
+    props: WebFlatListProps<T>,
     ref:
       | ((instance: FlatListType<T> | null) => void)
       | MutableRefObject<FlatListType<T> | null>
@@ -55,8 +72,11 @@ export const BidirectionalFlatList = (React.forwardRef(
       ListFooterComponent,
       ListHeaderComponent,
       onEndReached,
+      onEndReachedThreshold = 300,
+      onRefresh,
       onScroll,
       onStartReached,
+      onStartReachedThreshold = 300,
       renderItem,
       showDefaultLoadingIndicators = true,
     } = props;
@@ -65,39 +85,243 @@ export const BidirectionalFlatList = (React.forwardRef(
       ? propInitialScrollIndex
       : 0;
     const virtuosoRef = useRef<VirtuosoHandle>(null);
+    const scrollerRef = useRef<HTMLElement | null>(null);
     const offsetFromBottom = useRef(1000000);
+    const offsetFromTop = useRef(0);
     const prependingItems = useRef(false);
     const appendingItems = useRef(false);
-
     const [onStartReachedInProgress, setOnStartReachedInProgress] = useState(
       false
     );
     const [onEndReachedInProgress, setOnEndReachedInProgress] = useState(false);
-
     const [vData, setVDate] = useState(data);
-    const firstItemIndex = useRef(0);
+    const [firstItemIndex, setFirstItemIndex] = useState(10 ** 7);
     const previousDataLength = useRef(data?.length || 0);
+    const fadeAnimation = useRef(new Animated.Value(0)).current;
+    const pan = useRef(new Animated.ValueXY()).current;
+    const lastYValue = useRef(0);
+    const panResponderActive = useRef(false);
+    const resetPanHandlerAndScroller = () => {
+      if (scrollerRef.current) {
+        scrollerRef.current.style.overflow = 'auto';
+      }
+      panResponderActive.current = false;
+    };
+    const startReached = async () => {
+      if (
+        prependingItems.current ||
+        onStartReachedInProgress ||
+        (inverted && !onEndReached) ||
+        (!inverted && !onStartReached)
+      ) {
+        return;
+      }
 
-    const startReached = () => {
-      if (prependingItems.current) return;
       prependingItems.current = true;
       setOnStartReachedInProgress(true);
       if (inverted) {
-        onEndReached();
+        await onEndReached?.();
       } else {
-        onStartReached();
+        await onStartReached?.();
       }
+      setOnStartReachedInProgress(false);
     };
 
-    const endReached = () => {
+    const endReached = async () => {
+      if (
+        onEndReachedInProgress ||
+        (inverted && !onStartReached) ||
+        (!inverted && !onEndReached)
+      ) {
+        return;
+      }
+
       appendingItems.current = true;
       setOnEndReachedInProgress(true);
       if (inverted) {
-        onStartReached();
+        await onStartReached?.();
       } else {
-        onEndReached();
+        await onEndReached?.();
       }
+      setOnEndReachedInProgress(false);
     };
+
+    const panResponder = useRef(
+      PanResponder.create({
+        onMoveShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+          if (prependingItems.current || !scrollerRef.current) {
+            return;
+          }
+
+          if (offsetFromTop.current <= 0) {
+            // @ts-ignore
+            lastYValue.current = pan.y._value;
+          } else {
+            resetPanHandlerAndScroller();
+          }
+        },
+        onPanResponderMove: (_, gestureState) => {
+          if (!scrollerRef.current) return;
+
+          if (
+            offsetFromTop.current > 0 ||
+            prependingItems.current ||
+            (!panResponderActive.current && gestureState.dy < 0)
+          ) {
+            resetPanHandlerAndScroller();
+            return;
+          }
+
+          if (lastYValue.current + gestureState.dy / dampingFactor < 0) {
+            resetPanHandlerAndScroller();
+            return;
+          }
+
+          panResponderActive.current = true;
+          scrollerRef.current.style.overflow = 'hidden';
+          pan.setValue({
+            x: 0,
+            y: lastYValue.current + gestureState.dy / dampingFactor,
+          });
+
+          if (
+            lastYValue.current + gestureState.dy / dampingFactor < 35 &&
+            lastYValue.current + gestureState.dy / dampingFactor > -35
+          ) {
+            fadeAnimation.setValue(0);
+          } else {
+            fadeAnimation.setValue(1);
+          }
+        },
+        onPanResponderRelease: (_, gestureState) => {
+          if (!scrollerRef.current) return;
+
+          resetPanHandlerAndScroller();
+
+          if (
+            lastYValue.current + gestureState.dy > 35 &&
+            offsetFromTop.current <= 0
+          ) {
+            fadeAnimation.setValue(1);
+            Animated.spring(pan, {
+              toValue: { x: 0, y: 30 },
+              useNativeDriver: true,
+            }).start();
+
+            simulateRefreshAction.current?.();
+          } else {
+            fadeAnimation.setValue(0);
+            Animated.spring(pan, {
+              toValue: { x: 0, y: 0 },
+              useNativeDriver: true,
+            }).start();
+          }
+        },
+      })
+    ).current;
+
+    const Footer = () =>
+      ListFooterComponent ? (
+        <ListFooterComponent />
+      ) : onEndReachedInProgress ? (
+        FooterLoadingIndicator ? (
+          <FooterLoadingIndicator />
+        ) : showDefaultLoadingIndicators ? (
+          <View style={styles.indicatorContainer}>
+            <ActivityIndicator color={activityIndicatorColor} size={'small'} />
+          </View>
+        ) : null
+      ) : null;
+
+    const handleScroll: VirtuosoProps<unknown>['onScroll'] = (e) => {
+      const targetElem = e.target as HTMLDivElement;
+      offsetFromBottom.current =
+        targetElem.scrollHeight -
+        (targetElem.scrollTop + targetElem.clientHeight);
+      offsetFromTop.current = targetElem.scrollTop;
+
+      if (
+        inverted &&
+        !appendingItems.current &&
+        !prependingItems.current &&
+        startReached
+      ) {
+        if (targetElem.scrollTop <= onEndReachedThreshold) {
+          startReached();
+        }
+
+        if (offsetFromBottom.current <= onStartReachedThreshold && endReached) {
+          endReached();
+        }
+      } else {
+        if (targetElem.scrollTop <= onStartReachedThreshold && endReached) {
+          endReached();
+        }
+
+        if (offsetFromBottom.current <= onEndReachedThreshold && startReached) {
+          startReached();
+        }
+      }
+
+      onScroll?.(e);
+    };
+
+    const Header = () =>
+      ListHeaderComponent ? (
+        <ListHeaderComponent />
+      ) : onStartReachedInProgress ? (
+        HeaderLoadingIndicator ? (
+          <HeaderLoadingIndicator />
+        ) : showDefaultLoadingIndicators ? (
+          <View style={styles.indicatorContainer}>
+            <ActivityIndicator color={activityIndicatorColor} size={'small'} />
+          </View>
+        ) : null
+      ) : null;
+
+    const itemContent = (index: number) => {
+      if (!renderItem) {
+        console.warn('Please specify renderItem prop');
+        return null;
+      }
+
+      if (!vData) {
+        return null;
+      }
+
+      const indexInData = inverted
+        ? vData.length - 1 - (index - firstItemIndex)
+        : index - firstItemIndex;
+
+      return (
+        <>
+          {/* @ts-ignore */}
+          {renderItem({ index, item: vData[indexInData] })}
+          {indexInData !== vData.length && !!ItemSeparatorComponent && (
+            <ItemSeparatorComponent />
+          )}
+        </>
+      );
+    };
+
+    const onRefreshRef = useRef(onRefresh);
+
+    const simulateRefreshAction = useRef(async () => {
+      if (!scrollerRef.current || prependingItems.current) return;
+
+      prependingItems.current = true;
+      await waiter();
+      await onRefreshRef.current?.();
+      scrollerRef.current.style.overflow = 'auto';
+
+      Animated.spring(pan, {
+        toValue: { x: 0, y: 0 },
+        useNativeDriver: true,
+      }).start();
+
+      fadeAnimation.setValue(0);
+    });
 
     useEffect(() => {
       const updateVirtuoso = () => {
@@ -105,17 +329,17 @@ export const BidirectionalFlatList = (React.forwardRef(
           return;
         }
 
-        let nextFirstItemIndex = firstItemIndex.current;
+        let nextFirstItemIndex = firstItemIndex;
         if (prependingItems.current === true) {
           nextFirstItemIndex =
-            firstItemIndex.current - (data.length - previousDataLength.current);
+            firstItemIndex - (data.length - previousDataLength.current);
+          setFirstItemIndex(nextFirstItemIndex);
         }
 
-        prependingItems.current = false;
-        previousDataLength.current = data.length;
-
-        firstItemIndex.current = nextFirstItemIndex;
         setVDate(() => data);
+        prependingItems.current = false;
+        appendingItems.current = false;
+        previousDataLength.current = data.length;
 
         if (
           enableAutoscrollToTop &&
@@ -131,70 +355,7 @@ export const BidirectionalFlatList = (React.forwardRef(
       };
 
       updateVirtuoso();
-    }, [enableAutoscrollToTop, autoscrollToTopThreshold, data, setVDate]);
-
-    const Footer = () =>
-      ListFooterComponent ? (
-        <ListFooterComponent />
-      ) : onEndReachedInProgress ? (
-        FooterLoadingIndicator ? (
-          <FooterLoadingIndicator />
-        ) : showDefaultLoadingIndicators ? (
-          <View style={styles.indicatorContainer}>
-            <ActivityIndicator color={activityIndicatorColor} size={'small'} />
-          </View>
-        ) : null
-      ) : null;
-
-    const Header = () =>
-      ListHeaderComponent ? (
-        <ListHeaderComponent />
-      ) : onStartReachedInProgress ? (
-        HeaderLoadingIndicator ? (
-          <HeaderLoadingIndicator />
-        ) : showDefaultLoadingIndicators ? (
-          <View style={styles.indicatorContainer}>
-            <ActivityIndicator color={activityIndicatorColor} size={'small'} />
-          </View>
-        ) : null
-      ) : null;
-
-    const handleScroll: VirtuosoProps<unknown>['onScroll'] = (e) => {
-      const targetElem = e.target;
-      offsetFromBottom.current =
-        // @ts-ignore
-        targetElem.scrollHeight -
-        // @ts-ignore
-        (targetElem.scrollTop + targetElem.clientHeight);
-
-      // @ts-ignore
-      onScroll?.(e);
-    };
-
-    const itemContent = (index: number) => {
-      if (!renderItem) {
-        console.warn('Please specify renderItem prop');
-        return null;
-      }
-
-      if (!vData) {
-        return null;
-      }
-
-      const indexInData = inverted
-        ? vData.length - 1 - (index + Math.abs(firstItemIndex.current))
-        : index + Math.abs(firstItemIndex.current);
-
-      return (
-        <>
-          {/* @ts-ignore */}
-          {renderItem({ index, item: vData[indexInData] })}
-          {indexInData !== vData.length && !!ItemSeparatorComponent && (
-            <ItemSeparatorComponent />
-          )}
-        </>
-      );
-    };
+    }, [enableAutoscrollToTop, autoscrollToTopThreshold, data, firstItemIndex]);
 
     useImperativeHandle(
       ref,
@@ -208,16 +369,17 @@ export const BidirectionalFlatList = (React.forwardRef(
         scrollToEnd: ({ animated }: { animated?: boolean | null }) => {
           if (!vData?.length) return;
 
-          // eslint-disable-next-line babel/no-unused-expressions
-          inverted
-            ? virtuosoRef.current?.scrollToIndex({
-                behavior: animated ? 'smooth' : 'auto',
-                index: 0,
-              })
-            : virtuosoRef.current?.scrollToIndex({
-                behavior: animated ? 'smooth' : 'auto',
-                index: vData.length - 1,
-              });
+          if (inverted) {
+            virtuosoRef.current?.scrollToIndex({
+              behavior: animated ? 'smooth' : 'auto',
+              index: 0,
+            });
+          } else {
+            virtuosoRef.current?.scrollToIndex({
+              behavior: animated ? 'smooth' : 'auto',
+              index: vData.length - 1,
+            });
+          }
         },
         scrollToIndex: ({
           animated,
@@ -230,16 +392,17 @@ export const BidirectionalFlatList = (React.forwardRef(
             return;
           }
 
-          // eslint-disable-next-line babel/no-unused-expressions
-          inverted
-            ? virtuosoRef.current?.scrollToIndex({
-                behavior: animated ? 'smooth' : 'auto',
-                index: vData.length - 1 - index,
-              })
-            : virtuosoRef.current?.scrollToIndex({
-                behavior: animated ? 'smooth' : 'auto',
-                index,
-              });
+          if (inverted) {
+            virtuosoRef.current?.scrollToIndex({
+              behavior: animated ? 'smooth' : 'auto',
+              index: vData.length - 1 - index,
+            });
+          } else {
+            virtuosoRef.current?.scrollToIndex({
+              behavior: animated ? 'smooth' : 'auto',
+              index,
+            });
+          }
         },
         scrollToItem: ({
           animated,
@@ -264,29 +427,85 @@ export const BidirectionalFlatList = (React.forwardRef(
       [virtuosoRef, inverted, vData]
     );
 
+    useEffect(() => {
+      onRefreshRef.current = onRefresh;
+    }, [onRefresh]);
+
     if (!vData?.length || vData?.length === 0) {
       // @ts-ignore
       return <ListEmptyComponent />;
     }
 
+    if (!onRefresh) {
+      return (
+        <Virtuoso<T>
+          components={{
+            Footer,
+            Header,
+          }}
+          endReached={endReached}
+          firstItemIndex={firstItemIndex}
+          initialTopMostItemIndex={
+            inverted
+              ? vData.length - 1 - initialScrollIndex
+              : initialScrollIndex
+          }
+          itemContent={itemContent}
+          onScroll={handleScroll}
+          overscan={300}
+          ref={virtuosoRef}
+          // @ts-ignore
+          scrollerRef={(ref) => (scrollerRef.current = ref)}
+          startReached={startReached}
+          totalCount={vData.length}
+        />
+      );
+    }
+
     return (
-      <Virtuoso<T>
-        components={{
-          Footer,
-          Header,
-        }}
-        endReached={endReached}
-        firstItemIndex={firstItemIndex.current}
-        followOutput={enableAutoscrollToTop ? 'smooth' : false}
-        initialTopMostItemIndex={
-          inverted ? vData.length - 1 - initialScrollIndex : initialScrollIndex
-        }
-        itemContent={itemContent}
-        onScroll={handleScroll}
-        ref={virtuosoRef}
-        startReached={startReached}
-        totalCount={vData.length}
-      />
+      <>
+        <Animated.View
+          style={{
+            flex: 1,
+            transform: [{ translateX: 0 }, { translateY: pan.y }],
+          }}
+          {...panResponder.panHandlers}
+        >
+          <Animated.View
+            style={[
+              {
+                opacity: fadeAnimation,
+                transform: [{ translateX: 0 }, { translateY: -30 }],
+              },
+              styles.refreshControl,
+            ]}
+          >
+            <ActivityIndicator />
+          </Animated.View>
+
+          <Virtuoso<T>
+            components={{
+              Footer,
+              Header,
+            }}
+            endReached={endReached}
+            firstItemIndex={firstItemIndex}
+            initialTopMostItemIndex={
+              inverted
+                ? vData.length - 1 - initialScrollIndex
+                : initialScrollIndex
+            }
+            itemContent={itemContent}
+            onScroll={handleScroll}
+            overscan={300}
+            ref={virtuosoRef}
+            // @ts-ignore
+            scrollerRef={(ref) => (scrollerRef.current = ref)}
+            startReached={startReached}
+            totalCount={vData.length}
+          />
+        </Animated.View>
+      </>
     );
   }
 ) as unknown) as BidirectionalFlatListType;
